a3b917b44846f719851a2cab3c3b19cb309df19b
DuongLT <letrungduongxx3@gmail.com>
2021-06-24 03:04:23 +0700

    Enable react native screens

--- index.js
+++ index.js
@@ -5,5 +5,7 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import { enableScreens } from 'react-native-screens';
+enableScreens();
 
 AppRegistry.registerComponent(appName, () => App);
