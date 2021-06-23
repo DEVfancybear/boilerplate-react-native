d870a566f91ed5198ab62e967eb5ec061f21bedf
DuongLT <letrungduongxx3@gmail.com>
2021-06-24 03:59:55 +0700

    Config lib react-native-keyboard-manager

--- src/Endpoint.tsx
+++ src/Endpoint.tsx
@@ -10,15 +10,39 @@
 
 import React from 'react';
 import {useTranslation} from 'react-i18next';
-import {ScrollView, StyleSheet, Text, useColorScheme, View} from 'react-native';
-
 import {
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-import './i18n/';
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import KeyboardManager from 'react-native-keyboard-manager';
 
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import './i18n/';
+if (Platform.OS === 'ios') {
+  KeyboardManager.setEnable(true);
+  KeyboardManager.setEnableDebugging(false);
+  KeyboardManager.setKeyboardDistanceFromTextField(10);
+  KeyboardManager.setEnableAutoToolbar(false);
+  // KeyboardManager.setToolbarDoneBarButtonItemText("Done");
+  // KeyboardManager.setToolbarManageBehaviourBy("subviews"); // "subviews" | "tag" | "position"
+  // KeyboardManager.setToolbarPreviousNextButtonEnable(false);
+  // KeyboardManager.setToolbarTintColor('#0000FF'); // Only #000000 format is supported
+  // KeyboardManager.setToolbarBarTintColor('#FFFFFF'); // Only #000000 format is supported
+  // KeyboardManager.setShouldShowToolbarPlaceholder(true);
+  KeyboardManager.setOverrideKeyboardAppearance(true);
+  KeyboardManager.setKeyboardAppearance('default'); // "default" | "light" | "dark"
+  KeyboardManager.setShouldResignOnTouchOutside(true);
+  KeyboardManager.setShouldPlayInputClicks(true);
+  KeyboardManager.resignFirstResponder();
+  // KeyboardManager.isKeyboardShowing()
+  //   .then((isShowing) => {
+  //       // ...
+  //   });
+}
 const Endpoint = () => {
   const {t} = useTranslation();
   const isDarkMode = useColorScheme() === 'dark';
